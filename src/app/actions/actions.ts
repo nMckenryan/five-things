b1869f1016db65eba7547159b5851967f4fdffe9
nMckenryan <n.mckenryan@gmail.com>
2024-07-10 18:09:24 +1000

    progress - fix duplicate pushing on load

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -4,20 +4,24 @@ import { db } from "~/server/db";
 import { posts } from "~/server/db/schema";
 
  
-export async function createPost(subjectName: string, fiveThing1: string, fiveThing2: string, fiveThing3: string, fiveThing4: string, fiveThing5: string) {
+export async function createPost(subjectName: string, fiveThing1: string, fiveThing2: string, fiveThing3: string, fiveThing4: string, fiveThing5: string, userName: string) {
     const agreeCount = 0; 
     const disagreeCount = 0;
-    const userName = "jim";
-    
-    await db.insert(posts).values({
-      subjectName,
-      agreeCount,
-      disagreeCount,
-      fiveThing1,
-      fiveThing2,
-      fiveThing3,
-      fiveThing4,
-      fiveThing5,
-      userName
-    });
+
+    try {
+        await db.insert(posts).values({
+            subjectName,
+            agreeCount,
+            disagreeCount,
+            fiveThing1,
+            fiveThing2,
+            fiveThing3,
+            fiveThing4,
+            fiveThing5,
+            userName
+          });
+    } catch (error) {
+        console.log("Could not insert post: ", error);
+    }
+
 }
