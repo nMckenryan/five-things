52f1171f6934313347c3b088e226c0f2d82c46ac
nMckenryan <n.mckenryan@gmail.com>
2024-07-21 03:29:53 +1000

    Interim fix to access edit button

--- src/app/components/bullet-card-modal.tsx
+++ src/app/components/bullet-card-modal.tsx
@@ -3,7 +3,9 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import '../../styles/bullet-card.css';
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { deletePost } from "~/server/queries";
+import Link from "next/link";
 
 interface Props {
   subjectName: string;
@@ -24,25 +26,31 @@ export default function BulletCardModal(props: Props) {
               await deletePost(props.postId );
             }
           }>
-          <CardContent style={{paddingTop: "0px", paddingBottom: "0px"}}>        
-
-
-            <Button className="closeButton" type="submit" ><DeleteIcon/></Button>
-
-
-            <Typography variant="h5" component="div" className="subjectName">
-              {props.subjectName}
-            </Typography>            
-              <ul className="goodBullets">
-                {props.fiveGoodThings.map(thing => <li key={thing.indexOf(thing)}>{thing.toString()}</li>)}
-              </ul>
-
-          </CardContent>
-          <CardActions className="opinionCountMain"  >
-            <div className="opinionCount"><ThumbUpIcon/><p>&nbsp;{props.agreeCount}</p></div>
-            <div className="opinionCount"><p>&nbsp;{props.userId}</p></div>
-            <div className="opinionCount"><ThumbDownIcon/><p>&nbsp; {props.disagreeCount}</p></div>
-          </CardActions></form>
+            <CardContent style={{paddingTop: "0px", paddingBottom: "0px"}}>        
+             
+              <Typography variant="h5" component="div" className="subjectName">
+                {props.subjectName}
+              </Typography>            
+                <ul className="goodBullets">
+                  {props.fiveGoodThings.map(thing => <li key={thing.indexOf(thing)}>{thing.toString()}</li>)}
+                </ul>
+            </CardContent>
+            
+            <CardActions className="opinionCountMain"  >
+              <div style={{display: "flex", flexDirection: "row", gap: "1rem"}}>
+              <Button className="closeButton" type="submit" ><DeleteIcon/></Button>
+              <div> </div>
+              <Link key={props.postId} href={`/post/${props.postId}/edit`}>
+                <Button className="editButton"><EditIcon/></Button>
+              </Link>
+              </div>
+              <div style={{display: "flex", flexDirection: "row", gap: "1rem"}}>
+              <div className="opinionCount"><ThumbUpIcon/><p>&nbsp;{props.agreeCount}</p></div>
+              <div className="opinionCount"><p>&nbsp;{props.userId}</p></div>
+              <div className="opinionCount"><ThumbDownIcon/><p>&nbsp; {props.disagreeCount}</p></div>
+              </div>
+            </CardActions>
+          </form>
         </Card>
       );
 }
