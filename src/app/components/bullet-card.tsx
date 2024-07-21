37307ddf48586e92af03e7c39f214e1ac2f92f7b
nMckenryan <n.mckenryan@gmail.com>
2024-07-21 12:45:44 +1000

    Set up confirmation toast

--- src/app/components/bullet-card.tsx
+++ src/app/components/bullet-card.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, Typography, CardActions } from "@mui/material";
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import '../../styles/bullet-card.css';
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import "../../styles/bullet-card.css";
 
 interface Props {
   subjectName: string;
@@ -12,21 +12,44 @@ interface Props {
 }
 
 export default function BulletCard(props: Props) {
-    return (
-        <Card className="fiveCard" sx={{ background: "#EDFA8B", boxShadow: "5px 5px 5px 0px rgba(0,0,0,0.5)"}}>
-          <CardContent style={{ marginTop: "5px",paddingTop: "2px", paddingBottom: "0px"}}> 
-            <Typography variant="h5" component="div" className="subjectName">
-              {props.subjectName}
-            </Typography>            
-              <ul className="goodBullets">
-                {props.fiveGoodThings.map(thing => <li key={thing.indexOf(thing)}>{thing.toString()}</li>)}
-              </ul>
-
-          </CardContent>
-          <CardActions className="opinionCountMain" style={{paddingTop: "1px", paddingBottom: "0px"}}  >
-            <div className="opinionCount"><ThumbUpIcon/><p>&nbsp;{props.agreeCount}</p></div>
-            <div className="opinionCount"><ThumbDownIcon/><p>&nbsp; {props.disagreeCount}</p></div>
-          </CardActions>
-        </Card>
-      );
+  return (
+    <Card
+      className="fiveCard"
+      sx={{
+        background: "#EDFA8B",
+        boxShadow: "5px 5px 5px 0px rgba(0,0,0,0.5)",
+      }}
+    >
+      <CardContent
+        style={{ marginTop: "5px", paddingTop: "2px", paddingBottom: "0px" }}
+      >
+        <Typography variant="h5" component="div" className="subjectName">
+          {props.subjectName}
+        </Typography>
+        <ul className="goodBullets">
+          {props.fiveGoodThings.map((thing) => (
+            <li key={thing.indexOf(thing)}>{thing.toString()}</li>
+          ))}
+        </ul>
+      </CardContent>
+      <CardActions
+        style={{
+          paddingTop: "1px",
+          paddingBottom: "0px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="opinionCount">
+          <ThumbUpIcon />
+          <p>&nbsp;{props.agreeCount}</p>
+        </div>
+        <div className="opinionCount">
+          <ThumbDownIcon />
+          <p>&nbsp; {props.disagreeCount}</p>
+        </div>
+      </CardActions>
+    </Card>
+  );
 }
--- src/app/components/insert-card.tsx
+++ src/app/components/insert-card.tsx
@@ -1,101 +1,246 @@
-"use client"; 
+"use client";
 
 import { Card, CardContent, Typography, CardActions } from "@mui/material";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
-import Modal from '@mui/material/Modal';
+import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import "../../styles/insert-card.css";
-import { useUser } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/dist/client/components/redirect";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { createPost } from "../actions/actions";
+import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
+import { string } from "zod";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
   boxShadow: 24,
-  p: 2
+  p: 2,
 };
 
+interface Props {
+  subjectName: string;
+  firstThing: string;
+  secondThing: string;
+  thirdThing: string;
+  fourthThing: string;
+  fifthThing: string;
+  postId: number;
+}
 
-export default function InsertCard({ createPost }: { createPost: (subjectName: string, fiveThing1: string, fiveThing2: string, fiveThing3: string, fiveThing4: string, fiveThing5: string) => Promise<void> }) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+interface IFormInputs {
+  subjectName: string;
+  firstThing: string;
+  secondThing: string;
+  thirdThing: string;
+  fourthThing: string;
+  fifthThing: string;
+}
 
-  const [formData, setFormData] = React.useState({
-    subjectName: "",
-    n1: "",
-    n2: "",
-    n3: "",
-    n4: "",
-    n5: "",
-  });
+export default function InsertCard(props: Props) {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const handleOpen = () => setModalOpen(true);
+  const handleClose = () => setModalOpen(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
+  const [toastState, setToastState] = React.useState(false);
+  const [toastMessage, setToastMessage] = React.useState("");
+
+  const handleToastOpen = () => {
+    setToastState(true);
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await createPost(formData.subjectName, formData.n1, formData.n2, formData.n3, formData.n4, formData.n5);
-    handleClose();
-
+  const handleToastClose = () => {
+    setToastState(false);
   };
 
-    return (
-      <>
-        <AddCircleIcon sx={{ color: "white" }} onClick={handleOpen}/>
+  const { handleSubmit, control } = useForm<IFormInputs>();
+  const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+    console.log("Added", data);
+    try {
+      await createPost(
+        data.subjectName,
+        data.firstThing,
+        data.secondThing,
+        data.thirdThing,
+        data.fourthThing,
+        data.fifthThing
+      );
+      setToastMessage("Post Created!");
+    } catch (error) {
+      setToastMessage("Error: " + String(error));
+    }
+    handleToastOpen();
+  };
 
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
+  return (
+    <>
+      <AddCircleIcon sx={{ color: "white" }} onClick={handleOpen} />
+
+      <Modal
+        open={modalOpen}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={toastState}
+            onClose={handleToastClose}
+            message={toastMessage}
+          />
           <Card sx={style}>
-            <form onSubmit={handleSubmit}>
-              <CardContent>    
-                  <Stack
-                    sx={{
-                      width: '60ch'
-                    }}
-                    spacing={1}
-                  >            
-                    <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ textAlign: "center"}}>
-                      Add New Five Things!
-                    </Typography> 
-                    <TextField className="thingInput" label="Subject" name="subjectName" variant="outlined" color="success" onChange={handleChange} required />
-                    <TextField className="thingInput" label="Nice Thing 1" name="n1" variant="outlined" color="success" onChange={handleChange} required />
-                    <TextField className="thingInput" label="Nice Thing 2" name="n2" variant="outlined" color="success" onChange={handleChange} required/>
-                    <TextField className="thingInput" label="Nice Thing 3" name="n3" variant="outlined" color="success"  onChange={handleChange} required/>
-                    <TextField className="thingInput" label="Nice Thing 4" name="n4" variant="outlined" color="success" onChange={handleChange} required />
-                    <TextField className="thingInput" label="Nice Thing 5" name="n5" variant="outlined" color="success" onChange={handleChange} required />
-                  </Stack>
-                </CardContent>
-              <CardActions sx={{display: "flex", justifyContent: "space-between"}}>
-                <Button variant="outlined" startIcon={<DeleteIcon />}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <CardContent>
+                <Stack
+                  sx={{
+                    width: "60ch",
+                  }}
+                  spacing={1}
+                >
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                    sx={{ textAlign: "center" }}
+                  >
+                    Add New Five Things!
+                  </Typography>
+
+                  <Controller
+                    name="subjectName"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.subjectName}
+                    render={({ field }) => (
+                      <TextField
+                        label="Subject"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="firstThing"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.firstThing}
+                    render={({ field }) => (
+                      <TextField
+                        label="First Thing"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="secondThing"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.secondThing}
+                    render={({ field }) => (
+                      <TextField
+                        label="Second Thing"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="thirdThing"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.thirdThing}
+                    render={({ field }) => (
+                      <TextField
+                        label="Third Thing"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="fourthThing"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.fourthThing}
+                    render={({ field }) => (
+                      <TextField
+                        label="Fourth Thing"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+
+                  <Controller
+                    name="fifthThing"
+                    control={control}
+                    rules={{ required: true }}
+                    defaultValue={props.fifthThing}
+                    render={({ field }) => (
+                      <TextField
+                        label="Fifth Thing"
+                        className="thingInput"
+                        variant="outlined"
+                        color="success"
+                        required
+                        {...field}
+                      />
+                    )}
+                  />
+                </Stack>
+              </CardContent>
+              <CardActions
+                sx={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <Button
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  onClick={handleToastOpen}
+                >
                   Delete
                 </Button>
-                <Button variant="outlined" endIcon={<AddCircleIcon />} type="submit">
+                <Button
+                  variant="outlined"
+                  endIcon={<AddCircleIcon />}
+                  type="submit"
+                >
                   Send
                 </Button>
               </CardActions>
-          </form>
+            </form>
           </Card>
+        </div>
       </Modal>
-
-        </>
-      );
+    </>
+  );
 }
